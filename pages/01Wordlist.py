ba05187230f9cbef6f2d24590cb10d1e0209d371
MK <99416359+MK316@users.noreply.github.com>
2025-08-19 22:31:06 +0900

    Update 01Wordlist.py

--- pages/01Wordlist.py
+++ pages/01Wordlist.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Set up page
 st.set_page_config(page_title="Test App")
-st.title("🍰단어장")
+st.markdown("### 🍰 맛있는 단어장")
 
 # Load the CSV from GitHub
 CSV_URL = "https://raw.githubusercontent.com/jihyeon0531/WordApp/refs/heads/main/data/wdata01.csv"
