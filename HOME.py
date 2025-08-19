0199f4cb7619d4cf50ac665a0a79cec4c4770353
MK <99416359+MK316@users.noreply.github.com>
2025-08-19 22:19:08 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -2,3 +2,18 @@ import streamlit as st
 
 st.write("Welcome to Jihyeon Teacher's App Page")
 st.caption("Since Aug 15, 2025")
+
+# Image links
+main_image_url = "https://raw.githubusercontent.com/jihyeon0531/WordApp/main/images/main.png"
+qr_image_url = "https://raw.githubusercontent.com/jihyeon0531/WordApp/main/images/qr.png"
+
+# Center the images
+st.markdown(
+    f"""
+    <div style="text-align: center;">
+        <img src="{main_image_url}" width="300"><br><br>
+        <img src="{qr_image_url}" width="180">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
