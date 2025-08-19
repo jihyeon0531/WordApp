6f075f037e568ad266285c8dd34a0cd54d06898b
MK <99416359+MK316@users.noreply.github.com>
2025-08-19 22:26:06 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.write("Welcome to Jihyeon Teacher's App Page")
+st.markdown("### Welcome to Jihyeon Teacher's App Page")
 st.caption("Since Aug 15, 2025")
 
 # Image links
@@ -13,5 +13,5 @@ col1, col2, col3 = st.columns([1, 2, 1])  # middle column bigger
 with col1:
     st.image(main_image_url, width=500, caption="Welcome Image")  # Expand button appears
 with col3:
-    st.image(qr_image_url, width=50, caption="QR Code")  # Expand button appears
+    st.image(qr_image_url, width=50, caption="QR")  # Expand button appears
 
