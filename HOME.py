ed0ebf9cbbc6ef320c1428ab39dfaf9900f7af37
MK <99416359+MK316@users.noreply.github.com>
2025-08-19 22:21:52 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -7,13 +7,9 @@ st.caption("Since Aug 15, 2025")
 main_image_url = "https://github.com/jihyeon0531/WordApp/raw/main/images/welcome2.png"
 qr_image_url = "https://github.com/jihyeon0531/WordApp/raw/main/images/appQR.png"
 
-# Center the images
-st.markdown(
-    f"""
-    <div style="text-align: center;">
-        <img src="{main_image_url}" width="500"><br><br>
-        <img src="{qr_image_url}" width="100">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+# Use columns to center images
+col1, col2, col3 = st.columns([1, 2, 1])  # middle column bigger
+
+with col2:
+    st.image(main_image_url, width=500, caption="Welcome Image")  # Expand button appears
+    st.image(qr_image_url, width=120, caption="QR Code")  # Expand button appears
