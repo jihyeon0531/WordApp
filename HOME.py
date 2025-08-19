cbb6f1baf59baad7e05d51a691514636685a14c2
MK <99416359+MK316@users.noreply.github.com>
2025-08-19 22:25:00 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -13,5 +13,5 @@ col1, col2, col3 = st.columns([1, 2, 1])  # middle column bigger
 with col1:
     st.image(main_image_url, width=500, caption="Welcome Image")  # Expand button appears
 with col3:
-    st.image(qr_image_url, width=100, caption="QR Code")  # Expand button appears
+    st.image(qr_image_url, width=50, caption="QR Code")  # Expand button appears
 
