097596968020dcad05b472cc7597146e1cc5b2de
MK <99416359+MK316@users.noreply.github.com>
2025-08-19 22:23:53 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -10,7 +10,8 @@ qr_image_url = "https://github.com/jihyeon0531/WordApp/raw/main/images/appQR.png
 # Use columns to center images
 col1, col2, col3 = st.columns([1, 2, 1])  # middle column bigger
 
+with col1:
+    st.image(qr_image_url, width=100, caption="QR Code")  # Expand button appears
 with col2:
     st.image(main_image_url, width=500, caption="Welcome Image")  # Expand button appears
-with col3:
-    st.image(qr_image_url, width=100, caption="QR Code")  # Expand button appears
+
