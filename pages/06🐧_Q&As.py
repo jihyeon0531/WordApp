bf42c242e708105142ae7900592f332515a09c34
MK <99416359+MK316@users.noreply.github.com>
2025-08-21 15:22:35 +0900

    Update 06🐧_Q&As.py

--- pages/06🐧_Q&As.py
+++ pages/06🐧_Q&As.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.write("Padlet will appear here.")
+st.header("🐾 Q & As: on Padlet")
+st.write("This Padlet serves as a dynamic hub for Q & As regarding this app page.")
+st.components.v1.iframe("https://padlet.com/satang0531/2-q-a-fxt7nbn57od1vawc", width=700, height=600)
